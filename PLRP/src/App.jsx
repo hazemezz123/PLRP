2ee72212a09cda6d8d5841ffc3ecf380c3f322df
hazemezz123 <hazemezz988@gmail.com>
2025-03-14 19:30:03 +0200

    Commit

--- PLRP/src/App.jsx
+++ PLRP/src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import useStateFunction from "./components/useState.jsx";
+import UseStateFunction from "./components/useState.jsx";
 const App = () => {
   return (
     <section>
-      <h1>Counter</h1>
-      <useStateFunction />
+      <UseStateFunction />
     </section>
   );
 };
--- PLRP/src/components/useState.jsx
+++ PLRP/src/components/useState.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
-const [counter, setCounter] = useState(0);
 
-const increment = () => {
-  setCounter((prev) => prev + 1);
-};
-const useState = () => {
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setCounter((prev) => prev - 1);
+  };
   return (
-    <section>
-      <h1>Counter Application</h1>
+    <div style={{ width: "%100", margin: "40px" }}>
+      <h1 style={{ alignItems: "center" }}>Counter Application</h1>
       <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>decrement</button>
       <p>Counter : {counter}</p>
-    </section>
+    </div>
   );
 };
 
-export default useState;
+export default Counter;
