7004e0855712e669374bb2a44b092c0e3beb93b9
hazemezz123 <hazemezz988@gmail.com>
2025-03-18 19:12:28 +0200

    Commit branch

--- PLRP/src/App.jsx
+++ PLRP/src/App.jsx
@@ -1,34 +1,7 @@
 import React from "react";
-import ProfileCard from "./components/Info";
 
-function App() {
-  return (
-    <div className="container d-flex flex-wrap justify-content-center gap-4 mt-5">
-      <ProfileCard
-        name="Ahmed Khaled"
-        age={22}
-        jobTitle="Frontend Developer"
-        bio="I love coding and UI/UX design."
-        image="https://randomuser.me/api/portraits/men/32.jpg"
-      />
-
-      <ProfileCard
-        name="Sara Ali"
-        age={25}
-        jobTitle="Data Scientist"
-        bio="Passionate about AI and Machine Learning."
-        image="https://randomuser.me/api/portraits/women/45.jpg"
-      />
-
-      <ProfileCard
-        name="Omar Hassan"
-        age={28}
-        jobTitle="Backend Engineer"
-        bio="I enjoy working with databases and APIs."
-        image="https://randomuser.me/api/portraits/men/76.jpg"
-      />
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
