7730da51e29a60c178834601c51a82918def518f
hazemezz123 <hazemezz988@gmail.com>
2025-03-19 13:14:42 +0200

    >

--- PLRP/src/App.jsx
+++ PLRP/src/App.jsx
@@ -1,35 +1,7 @@
-import React, { useState, createContext } from "react";
-import Children from "./components/Children";
-import "./styles/context-components.css";
-
-export const ThemeContext = createContext();
-export const LanguageContext = createContext();
+import React from "react";
 
 const App = () => {
-  const [theme, setTheme] = useState("light");
-  const [language, setLanguage] = useState("English");
-
-  return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <LanguageContext.Provider value={{ language, setLanguage }}>
-        <div className="app-container">
-          <div className="app-header">
-            <h1>Theme & Language Settings</h1>
-            <button
-              className="reset-button"
-              onClick={() => {
-                setTheme("light");
-                setLanguage("English");
-              }}
-            >
-              Reset Settings
-            </button>
-          </div>
-          <Children />
-        </div>
-      </LanguageContext.Provider>
-    </ThemeContext.Provider>
-  );
+  return <div>ContextApi Test</div>;
 };
 
 export default App;
--- PLRP/src/components/ComponentA.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import ComponentB from "./ComponentB";
-import { createContext } from "react";
-import ButtonCounter from "./ButtonCounter";
-import "../styles/components.css";
-
-export const UserContext = createContext();
-
-// Rest of your component stays exactly the same
